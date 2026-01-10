fb1a0c655670356a8c3636b2a3f2ac09f7513569
SeanVnng <seanvanngoc@gmail.com>
2026-01-10 15:30:02 +0100

    v2 - Mettre en œuvre des fonctionnalités de gestion des amis
    Ajout de fonctionnalités de gestion des amis, notamment l'ajout d'amis par téléphone et la récupération de la liste d'amis.

--- Developper_Des_Applications_Communicantes/server.py
+++ Developper_Des_Applications_Communicantes/server.py
@@ -81,25 +81,29 @@ def handle_client(client_socket, addr):
                         clients[current_username] = client_socket
                         send_to_user(current_username, {"type": "LOGIN_REPLY", "success": True})
                     else:
-                         # Pour le login, on peut utiliser current_username si défini, sinon send direct
                          client_socket.send(json.dumps({"type": "LOGIN_REPLY", "success": False}).encode('utf-8'))
 
                 elif msg_type == "REGISTER":
                     user = msg.get("username")
                     pwd = msg.get("password")
                     success = db.create_user(user, pwd)
-                    # CORRECTION ICI : On répond directement au socket (car pas encore de current_username)
                     try:
                         client_socket.send(json.dumps({"type": "REGISTER_REPLY", "success": success}).encode('utf-8'))
                     except: pass
 
                 elif not current_username: continue
 
-                # --- FONCTIONNALITÉS AVANCÉES ---
+                # --- NOUVEAU SYSTÈME D'AMIS ---
+                
+                elif msg_type == "ADD_FRIEND":
+                    phone = msg.get("phone")
+                    success, message = db.add_friend_by_phone(current_username, phone)
+                    send_to_user(current_username, {"type": "ADD_FRIEND_REPLY", "success": success, "message": message})
 
-                elif msg_type == "FIND_BY_PHONE":
-                    target_user = db.get_username_by_phone(msg.get("phone"))
-                    send_to_user(current_username, {"type": "FIND_PHONE_REPLY", "username": target_user})
+                elif msg_type == "GET_FRIENDS":
+                    # On ne renvoie plus tout le monde, seulement les amis
+                    friends = db.get_my_friends(current_username)
+                    send_to_user(current_username, {"type": "FRIENDS_LIST", "data": friends})
 
                 elif msg_type == "GET_TARGET_INFO":
                     target = msg.get("target")
@@ -158,7 +162,6 @@ def handle_client(client_socket, addr):
                                 "file_path": file_path
                             })
 
-                # --- APPELS & LOGS ---
                 elif msg_type == "CALL_REQUEST":
                     target = msg.get("target")
                     if target in clients:
@@ -172,15 +175,18 @@ def handle_client(client_socket, addr):
                     if target in clients:
                         send_to_user(target, {"type": "CALL_ENDED", "sender": current_username})
 
-                # --- STANDARD ---
                 elif msg_type == "GET_PROFILE":
                     send_to_user(current_username, {"type": "PROFILE_DATA", "data": db.get_user_profile(current_username)})
                 elif msg_type == "UPDATE_PROFILE":
                     db.update_user_profile(current_username, msg.get("infos"), msg.get("phone"))
+
                 elif msg_type == "HISTORY":
-                    send_to_user(current_username, {"type": "HISTORY_REPLY", "target": msg.get("target"), "data": db.get_conversation_history(current_username, msg.get("target"))})
+                    target = msg.get("target")
+                    db.mark_as_read(current_username, target)
+                    send_to_user(current_username, {"type": "HISTORY_REPLY", "target": target, "data": db.get_conversation_history(current_username, target)})
+
                 elif msg_type == "GET_CONVERSATIONS":
-                    send_to_user(current_username, {"type": "CONVERSATIONS_LIST", "data": db.get_user_conversations(current_username)})
+                    send_to_user(current_username, {"type": "CONVERSATIONS_LIST", "data": db.get_user_conversations_rich(current_username)})
 
             except json.JSONDecodeError: pass
         except: break
